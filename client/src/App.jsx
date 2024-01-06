04e2d5b4313bc40fb0fa5c2ec2b73f4bb7c295dc
Rubi Thapa <habi.thapa@gmail.com>
2024-01-06 09:41:01 +0545

    Send and recieve data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:3001");
+const App = () => {
+  const [message, setMessage] = useState("");
+  const [messageRecieved, setMessageRecieved] = useState("");
 
-function App() {
-  const [count, setCount] = useState(0)
+  const sendMessage = () => {
+    socket.emit("send_message", { message });
+  };
 
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageRecieved(data.message);
+    });
+  }, [socket]);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        placeholder="Type message here"
+        style={{ marginRight: 10 }}
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }}
+      />
+      <button onClick={sendMessage} style={{ padding: 3.5 }}>
+        Message
+      </button>
+      <h1>Message</h1>
+      {messageRecieved}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
